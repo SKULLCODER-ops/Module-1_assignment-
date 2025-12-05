44aa8525c66488a110ff823aa8171f02a9763ceb
SKULLCODER-ops <anlohrman13@gmail.com>
2025-12-05 17:34:18 -0500

    Add files via upload

---
+++ Module1_Assignment.py
@@ -0,0 +1,18 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> >>> # Module 1 Assignment
+... >>> # This program prints the sentence "It is raining cats and dogs."
+... >>> # Variables are used to store words and then combined into a full sentence.
+... >>>
+... >>> # Variable c stores the word "cats"
+... >>> c = "cats"
+... >>>
+... >>> # Variable d stores the word "dogs"
+... >>> d = "dogs"
+... >>>
+... >>> # Variable s combines the sentence with c and d
+... >>> s = "It is raining " + c + " and " + d
+... >>>
+... >>> # Print the final sentence
+... >>> print(s)
+... It is raining cats and dogs
